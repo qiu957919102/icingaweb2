a224ca9d25d5538fff33b3d612d773c2f996e5a2
Johannes Meyer <johannes.meyer@netways.de>
2016-11-21 12:10:58 +0100

    Introduce class CatalogStatistics
    refs #13013

---
+++ modules/translation/library/Translation/Catalog/CatalogStatistics.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Icinga\Module\Translation\Catalog;
+
+
+class CatalogStatistics
+{
+    public function countEntries()
+    {
+
+    }
+
+    public function countObsoleteEntries()
+    {
+
+    }
+
+    public function countMessages()
+    {
+
+    }
+
+    public function countFuzzyMessages()
+    {
+
+    }
+
+    public function countFaultyMessages()
+    {
+
+    }
+
+    public function refresh()
+    {
+
+    }
+}
